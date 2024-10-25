24fec536d3554cd3f2afa12b97494cc7dad44148
ipanzez <117064649+ipanzez@users.noreply.github.com>
2024-10-25 16:56:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_myweb
+from flask import Flask,render_templates
 import smtplib
 
 app=Flask(__name__)
@@ -6,7 +6,7 @@ app=Flask(__name__)
 #route() decorators
 @app.route('/')
 def home():
-    return render_myweb('index.html')
+    return render_templates('index.html')
 
 if __name__=='__main__':
     app.run(debug=True)
